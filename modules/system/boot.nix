ddec8f418f51fecc9ce27991601d207063929075
fettabit <143643888+fettabit@users.noreply.github.com>
2026-07-01 14:18:36 -0400

    feature: dual booting with GRUB and systemd disabled

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -1,6 +1,11 @@
-{ ... }:
-{
-    boot.loader.systemd-boot.enable = true;
-    boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.systemd-boot.configurationLimit = 10;
+{...}: {
+  boot.loader.systemd-boot.enable = false;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub = {
+    enable = true;
+    efiSupport = true;
+    device = "nodev";
+    useOSProber = true;
+    configurationLimit = 5;
+  };
 }

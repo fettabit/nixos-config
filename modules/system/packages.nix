4a9a337f4f87ba2f06d9e71036792cf0c9a6d034
fettabit <143643888+fettabit@users.noreply.github.com>
2026-07-03 22:08:49 -0400

    chore: replace hyprpaper with awww

--- modules/system/packages.nix
+++ modules/system/packages.nix
@@ -1,27 +1,26 @@
-{ pkgs, ... }:
-{
-    environment.systemPackages = with pkgs; [
-        vim
-        wget
-        unzip
-        curl
-        btop
-        kitty
-        python3
-        nautilus
-        waybar
-        fastfetch
-        hyprpolkitagent
-        grim
-        slurp
-        claude-code
-        hyprpaper
-        rofi
-        playerctl
-        alejandra
-        swaynotificationcenter
-        networkmanagerapplet
-        wl-clipboard
-        tree
-    ];
+{pkgs, ...}: {
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    unzip
+    curl
+    btop
+    kitty
+    python3
+    nautilus
+    waybar
+    fastfetch
+    hyprpolkitagent
+    grim
+    slurp
+    claude-code
+    awww
+    rofi
+    playerctl
+    alejandra
+    swaynotificationcenter
+    networkmanagerapplet
+    wl-clipboard
+    tree
+  ];
 }
